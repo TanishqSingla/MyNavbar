b74d22f736bb827a4683953ae9b0ed9ab0460963
TanishqSingla <tanishqsingla00@gmail.com>
2020-02-05 23:22:59 +0530

    done

---
+++ js.js
@@ -0,0 +1,10 @@
+function slide() {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("slide");
+  nav.classList.toggle("slided");
+}
+function rotate() {
+  var box = document.getElementById("box");
+  box.classList.toggle("rotate");
+  box.classList.toggle("rotated");
+}
